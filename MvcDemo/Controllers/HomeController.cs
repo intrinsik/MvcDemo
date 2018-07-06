410eca8cc323601864edf90f31d4a14bde98873e
German Aguinaga <intrinsik@cox.net>
2018-07-05 22:15:17 -0700

    Initial checkin of project

---
+++ MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewData["Message"] = "Welcome to ASP.NET. This message is brought to you by the 'ViewData[Message]' collection passed in by the the HomeController on the About method";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
